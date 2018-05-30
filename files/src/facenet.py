ba0c8c9d8a7f5ab527c2bc7177621d2c89df40fb
Jorge Santos <netneill@hotmail.com>
2018-05-30 16:24:43 -0600

    fix wrong operation divide a number in zero

--- files/src/facenet.py
+++ files/src/facenet.py
@@ -467,7 +467,6 @@ def calculate_accuracy(threshold, dist, actual_issame):
     return tpr, fpr, acc
 
 
-  
 def calculate_val(thresholds, embeddings1, embeddings2, actual_issame, far_target, nrof_folds=10, distance_metric=0, subtract_mean=False):
     assert(embeddings1.shape[0] == embeddings2.shape[0])
     assert(embeddings1.shape[1] == embeddings2.shape[1])
@@ -511,8 +510,17 @@ def calculate_val_far(threshold, dist, actual_issame):
     false_accept = np.sum(np.logical_and(predict_issame, np.logical_not(actual_issame)))
     n_same = np.sum(actual_issame)
     n_diff = np.sum(np.logical_not(actual_issame))
-    val = float(true_accept) / float(n_same)
-    far = float(false_accept) / float(n_diff)
+    
+    if n_same > 0:
+        val = float(true_accept) / float(n_same)
+    else:
+        val = 0
+    
+    if n_diff > 0:
+        far = float(false_accept) / float(n_diff)
+    else:
+        far = 0.0
+        
     return val, far
 
 def store_revision_info(src_path, output_dir, arg_string):
