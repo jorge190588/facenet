98c97511e0027456fd544f3f0bbd39be6d49d16b
jorgesantosn <netneill@hotmail.com>
2019-05-14 21:36:28 -0600

    refactor camera.py

--- files/DTL/camera.py
+++ files/DTL/camera.py
@@ -1,14 +1,7 @@
-import argparse
-import sys
-import cv2
-import numpy as np
-import os
+import  argparse, sys, cv2, numpy as np, os
 
 class Camera:
-    """ Point class represents and manipulates x,y coords. """
-
     def __init__(self):
-        """ Create a new point at the origin """
         self.first_name  = ""
         self.last_name  = ""
         self.folder=""
@@ -20,6 +13,7 @@ class Camera:
         self.path=""
         self.extension=""
         self.cameraNumber=0
+    
     def jpg_extension(self):
         self.extension=self.extension
 
@@ -27,8 +21,6 @@ class Camera:
         self.cap = cv2.VideoCapture(args.cameraNumber)  
         while True:
             ret,imageCaptured = self.cap.read()
-            
-            # show image
             cv2.imshow('Video', imageCaptured)
             k=cv2.waitKey(10)& 0xff
             if k==27:
@@ -37,8 +29,6 @@ class Camera:
             if (self.index <= self.maxFileNumber):
                 fileName = self.path+self.name+"_"+str(self.index)+self.extension
                 print('Full Name: ',fileName)
-
-                # save image
                 cv2.imwrite(fileName, imageCaptured)
                 self.index = self.index+1
             else:
@@ -48,7 +38,7 @@ class Camera:
     
     def initPath(self):
         if not (os.path.exists(self.path)):
-            print("path created")
+            print("path created: ",self.path)
             os.makedirs(self.path)
 
     def init(self, args):
@@ -81,6 +71,7 @@ if __name__ == '__main__':
     camera = Camera()
     args=parse_arguments(sys.argv[1:])
     camera.init(args)
+
 # example: py camera.py --first_name jorge --last_name santos --maxFileNumber 10 
 # example: py camera.py --first_name jorge --last_name santos --maxFileNumber 10 --directorioDeImagenes ..\imgs\imagenesDeEntrada
    
