c66b241834e84be7ac2fbe147c3a5be546ea8e5e
jorge190588 <netneill@hotmail.com>
2019-07-01 20:31:52 -0600

    save image from video when user press a key

--- files/DTL/video.py
+++ files/DTL/video.py
@@ -12,6 +12,8 @@ class Video:
         self.imageExtension = ""
         self.startInFrame=1
         self.imageName=""
+        self.saveWithAkey=0
+        self.indexFrame=0
 
     def jpg_extension(self):
         self.imageExtension=".jpg"
@@ -39,39 +41,49 @@ class Video:
         self.fullOutDirectory = os.path.join(self.path,self.outDirectory,self.imageName+"\\")
         self.cameraNumber = args.cameraNumber
         self.startInFrame=args.startInFrame
+        self.saveWithAkey= args.saveWithAkey
         self.initPath()
         self.capture()
 
     def capture(self):
+        print(self.videoFullPath)
         if (os.path.isfile(self.videoFullPath))==False:
             print("File does not exist")
             return False
         
         cap = cv2.VideoCapture(self.videoFullPath)
         indexFrame = 0 
+        print("press a to init and esc to quit")
         while True:
             ret,img = cap.read()
             cv2.imshow('Video', img)
             k=cv2.waitKey(10)& 0xff
             if k==27:
                 break
-            
-            indexFrame=indexFrame+1
-            if (indexFrame>self.startInFrame):
-                if (self.index < self.maxFileNumber):
-                    fullName = self.fullOutDirectory+self.imageName+"_"+str(self.index)+self.imageExtension
-                    print('full name ',fullName)
-                    # save image
-                    cv2.imwrite(fullName, img)
-                    self.index = self.index+1
+
+            if (self.saveWithAkey== 1):
+                if cv2.waitKey(33) == ord('a'):
+                    print("init to save frames")
+                    self.saveImage(img)
+            else: 
+                if (self.indexFrame>self.startInFrame):
+                    if (self.index < self.maxFileNumber):
+                        self.saveImage(img)
 
         cap.release()
         cv2.destroyAllWindows()
 
+    def saveImage(self,img):
+        self.indexFrame=self.indexFrame+1    
+        fullName = self.fullOutDirectory+self.imageName+"_"+str(self.index)+self.imageExtension
+        print('full name ',fullName)
+        cv2.imwrite(fullName, img)
+        self.index = self.index+1
+
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument('--videoFolder', type=str, help='Video folder', default="video")
-    parser.add_argument('videoName', type=str, help='Video name', default="videoName")
+    parser.add_argument('--videoName', type=str, help='Video name', default="videoName")
     parser.add_argument('--videoExtension', type=str, help='Video extension', default=".mp4")
     parser.add_argument('--outDirectory', type=str, help='directory', default='..\lfw\imagenesDeEntrada')
     parser.add_argument('--index', type=int, help='Index', default=1)
@@ -79,6 +91,7 @@ def parse_arguments(argv):
     parser.add_argument('--imageExtension', type=str, help='Image extension', default=".jpg")
     parser.add_argument('--cameraNumber', type=int, help='cameraNumber', default=0)
     parser.add_argument('--startInFrame', type=int, help='startInFrame', default=1)
+    parser.add_argument('--saveWithAkey', type=int, help='save image when the user pressed a key', default=0)
     parser.add_argument('--imageName', type=str, help='image name', default="")
     return parser.parse_args(argv)
 
